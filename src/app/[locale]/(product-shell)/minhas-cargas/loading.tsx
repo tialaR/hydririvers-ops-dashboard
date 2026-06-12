d7b91bc2b2f2a60a93b382ee516518b23f17f7d4
Tiala <tialarocha@gmail.com>
2026-06-11 23:58:49 -0300

    feat(cargo): add minhas-cargas loading state

---
+++ src/app/[locale]/(product-shell)/minhas-cargas/loading.tsx
@@ -0,0 +1,5 @@
+import { MyCargoesListSkeleton } from '@/features/cargo-market/components/my-cargoes-list/my-cargoes-list-skeleton';
+
+export default function MinhasCargasLoading() {
+  return <MyCargoesListSkeleton />;
+}
